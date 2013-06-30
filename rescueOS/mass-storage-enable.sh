5e15ca7d9cf48305c9aeecdb5e8cffd9d29d940c
NIN101 <NIN101@lavabit.com>
2013-06-30 12:12:04 +0200

    Detect whether internal emmc is mmcblk1 or mmcblk1 at boot and not when mass-storage-enable.sh is called

--- rescueOS/mass-storage-enable.sh
+++ rescueOS/mass-storage-enable.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 rmmod g_nokia 
-#stuff changes when sd card is inserted
-if [ -e /sys/block/mmcblk0 ] && [ -e /sys/block/mmcblk1 ] ; then 
-	modprobe g_file_storage file=/dev/mmcblk1p2,/dev/mmcblk1p1 stall=0 removable=1
-else
-	modprobe g_file_storage file=/dev/mmcblk0p2,/dev/mmcblk0p1 stall=0 removable=1
-fi
+nodenr=$(cat /run/internal_nodenr)
+modprobe g_file_storage file=/dev/mmcblk"$nodenr"p2,/dev/mmcblk"$nodenr"p1 stall=0 removable=1
+
 
 
