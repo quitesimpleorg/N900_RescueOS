50185d5620ef75e7028749e2227c2390bd0dcd2f
Daniel Gnoutcheff <daniel@gnoutcheff.name>
2015-03-29 13:30:17 -0400

    Kill /usr/share/getopt
    This directory had example scripts for util-linux's getopt.  They count
    as documentation and don't belong on an embedded rootfs.
    
    This also reduces the diff to RescueOS v1.1; this directory was
    (presumably manually) removed there as well.
    
    Also cleanup post-build.sh a bit while we're at it.

--- post-build.sh
+++ post-build.sh
@@ -4,11 +4,14 @@ set -e
 
 target="$1"
 
-delfiles=" \
-  $target/etc/init.d/rcK \
-  $target/etc/init.d/S20urandom \
-  $target/etc/init.d/S40network \
+delfiles="
+  $target/etc/init.d/rcK
+  $target/etc/init.d/S20urandom
+  $target/etc/init.d/S40network
   $target/etc/securetty"
+
 for file in $delfiles ; do 
-  [ -e "$file" ] && rm "$file" || true
+  [ ! -e "$file" ] || rm "$file"
 done
+
+[ ! -e "$target/usr/share/getopt" ] || rm -r "$target/usr/share/getopt"
