f725556ecdfc6b3d37c84291ed11c3bc0725d611
NIN101 <NIN101@quitesimple.org>
2015-05-31 13:30:41 +0200

    create the symlinks in post-build.sh.

--- post-build.sh
+++ post-build.sh
@@ -15,3 +15,14 @@ for file in $delfiles ; do
 done
 
 [ ! -e "$target/usr/share/getopt" ] || rm -r "$target/usr/share/getopt"
+
+ln -s "/rescueOS/usbnetworking-enable.sh" "$target/sbin/usbne"
+ln -s "/rescueOS/usbnetworking-disable.sh" "$target/sbin/usbnd"
+ln -s "/rescueOS/charge21.bash" "$target/sbin/chargebat"
+ln -s "/rescueOS/mount-maemo-root.sh" "$target/sbin/mmr"
+ln -s "/rescueOS/umount-maemo-root.sh" "$target/sbin/ummr"
+ln -s "/rescueOS/mass-storage-enable.sh" "$target/sbin/mse"
+ln -s "/rescueOS/mass-storage-disable.sh" "$target/sbin/msd"
+ln -s "/rescueOS/enableftp.sh" "$target/sbin/enftp"
+
+
